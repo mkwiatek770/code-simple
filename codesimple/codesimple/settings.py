b8c07ae7523a1bb8db484143404d90ff7db43cbf
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-26 09:53:11 +0100

    Added and configured PostgreSQL database

--- codesimple/codesimple/settings.py
+++ codesimple/codesimple/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'codesimple.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
