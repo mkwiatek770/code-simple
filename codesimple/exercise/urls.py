388d3dd82f233d0e294e1ea1fc00902e53bce0d2
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-27 13:51:02 +0100

    Created LandingPageView in views that is landing page of application. If user is authenticated he'll be redirect to exercise home page.

--- codesimple/exercise/urls.py
+++ codesimple/exercise/urls.py
@@ -1,4 +1,7 @@
+from django.urls import path
+from exercise import views
 
 urlpatterns = [
-
+    path("", views.LandingPageView.as_view(), name="index"),
+    path("home/", views.HomeView.as_view(), name="home"),
 ]
--- codesimple/exercise/views.py
+++ codesimple/exercise/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import (
+    TemplateView
+)
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+
+class LandingPageView(TemplateView):
+    template_name = "exercise/index.html"
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect(reverse("home"))
+        return render(request, self.template_engine)
+
+
+class HomeView(View):
+    def get(self, request):
+        pass
