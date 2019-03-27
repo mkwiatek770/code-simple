2d63a0431e3b46c7330247b86b042bcaa39a3a3b
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-27 10:55:14 +0100

    Added models: Tag, Exercise

--- codesimple/exercise/admin.py
+++ codesimple/exercise/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from exercise.models import Exercise, ExerciseTest, Tag
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExerciseTest)
+class ExerciseTestAdmin(admin.ModelAdmin):
+    pass
--- codesimple/exercise/models.py
+++ codesimple/exercise/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+
+    LEVELS = (
+        (1, "NEWBIE"),
+        (2, "BEGINNER"),
+        (3, "INTERMEDIATE"),
+        (4, "HARD"),
+        (5, "MASTER")
+    )
+
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+    image_name = models.CharField(max_length=100)
+    short_description = models.TextField(max_length=255)
+    description = RichTextField()
+    level = models.IntegerField(choices=LEVELS)
+    experience_points = models.PositiveIntegerField()
+    likes = models.SmallIntegerField(default=0)
+    dislikes = models.SmallIntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class ExerciseTest(models.Model):
+    test_number = models.IntegerField()
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    user_input = models.CharField(max_length=255)
+    expected_output = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.exercise.title} {self.user_input} --> {self.expected_output}"
