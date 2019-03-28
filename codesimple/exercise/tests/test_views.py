d8d64ee09f4b28966f3e575e2cad527bf194be90
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-28 11:08:12 +0100

    HomeView and LandingPage view with unit tests

---
+++ codesimple/exercise/tests/test_views.py
@@ -0,0 +1,131 @@
+from django.test import TestCase
+from django.urls import reverse
+from users.models import ProfileUser, Badge
+from exercise.models import Tag, Exercise
+
+LANDING_URL = reverse("index")
+HOME_URL = reverse("home")
+
+
+class TestLandingPageView(TestCase):
+    """Tests for LandingPageView"""
+
+    def setUp(self):
+        self.user = ProfileUser.objects.create(
+            username="username",
+            password="password"
+        )
+
+    def test_not_logged_user(self):
+        """Test GET method for logged user"""
+        response = self.client.get(LANDING_URL, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "exercise/index.html")
+
+    def test_logged_user(self):
+        """Test GET method for not logged user"""
+        self.client.force_login(self.user)
+        response = self.client.get(LANDING_URL, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, HOME_URL)
+        self.assertTemplateUsed(response, "exercise/home.html")
+
+
+class TestHomeView(TestCase):
+    """Tests for HomeView"""
+
+    def create_tag(self, name):
+        """Create tag with given name"""
+        return Tag.objects.create(
+            name=name
+        )
+
+    def create_exercise_with_tag(self, title, tag):
+        exercise = Exercise.objects.create(
+            title=title,
+            image_name="image",
+            short_description="short",
+            description="description",
+            level=1,
+            experience_points=100
+        )
+        exercise.tags.add(tag)
+        exercise.save()
+        return exercise
+
+    def create_badge(self):
+        return Badge.objects.create(
+            title="First Achieve",
+            description="desc..",
+            reward_points=100,
+            icon="icon.png"
+        )
+
+    def setUp(self):
+        self.badge = self.create_badge()
+        self.user = ProfileUser.objects.create(
+            username="username",
+            password="password",
+            email="email@gmail.com"
+        )
+
+    def test_get_logged_in(self):
+        """Test GET method for logged user """
+
+        self.client.force_login(self.user)
+        response = self.client.get(HOME_URL, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        # first_login achievement
+
+        self.assertIn("achievements", response.context)
+        self.assertEqual(response.context["achievements"][0], self.badge)
+
+    def test_get_not_logged_in(self):
+        """Test GET method for not logged user"""
+
+        response = self.client.get(HOME_URL, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, LANDING_URL)
+        self.assertTemplateUsed(response, "exercise/index.html")
+
+    def test_post_filter(self):
+        """Test POST method for filtering exercises"""
+
+        self.client.force_login(self.user)
+        t1 = self.create_tag("name1")
+        t2 = self.create_tag("name2")
+        exercise1 = self.create_exercise_with_tag("exercise1", t1)
+        exercise2 = self.create_exercise_with_tag("exercise2", t2)
+
+        response = self.client.post(HOME_URL, data={
+            "filter": [""],
+            "tag": "{}".format(t1.id),
+            "level": "all",
+            "language": "all"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(exercise1, response.context["exercises"])
+        self.assertNotIn(exercise2, response.context["exercises"])
+
+    def test_post_search(self):
+        """Test POST method for searching exercises"""
+
+        self.client.force_login(self.user)
+        t1 = self.create_tag("name1")
+        t2 = self.create_tag("name2")
+        exercise1 = self.create_exercise_with_tag("exercise1", t1)
+        exercise2 = self.create_exercise_with_tag("exercise2", t2)
+
+        response = self.client.post(HOME_URL, data={
+            "search": [""],
+            "phrase": "exercise1"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(exercise1, response.context["exercises"])
+        self.assertNotIn(exercise2, response.context["exercises"])
--- codesimple/exercise/views.py
+++ codesimple/exercise/views.py
@@ -1,9 +1,56 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import (
-    TemplateView
+    TemplateView,
 )
-from django.urls import reverse, reverse_lazy
+from exercise.models import (
+    Exercise,
+    ExerciseUser,
+    Tag
+)
+from users.models import (
+    Badge,
+    UserMessage
+)
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+
+
+def achievement_check(request):
+    user = request.user
+    user_badges = user.badges.all()
+    achievements = []
+
+    # First Achieve
+    badge0 = Badge.objects.filter(title="First Achieve")
+    if badge0 and badge0[0] not in user_badges:
+        achievements.append(badge0[0])
+
+    # Socialize
+    badge1 = Badge.objects.filter(title="Socialize")
+    if (badge1 and badge1[0] not in user_badges and
+            len(UserMessage.objects.filter(sender=user)) >= 1):
+        achievements.append(badge1[0])
+
+    # Socialize more
+    badge2 = Badge.objects.filter(title="Socialize More")
+    if (badge2 and badge2[0] not in user_badges and
+            len(UserMessage.objects.filter(sender=user)) >= 3):
+        achievements.append(badge2[0])
+
+    # Gettin' Started
+    badge3 = Badge.objects.filter(title="Gettin' Started")
+    if (badge3 and badge3[0] not in user_badges and
+            len(ExerciseUser.objects.filter(user=user, submitted=True)) >= 1):
+        achievements.append(badge3[0])
+    # Good Streak
+    badge4 = Badge.objects.filter(title="Good Streak")
+    if (badge4 and badge4[0] not in user_badges and
+            len(ExerciseUser.objects.filter(user=user, submitted=True)) >= 3):
+        achievements.append(badge4[0])
+
+    return achievements
 
 
 class LandingPageView(TemplateView):
@@ -15,6 +62,90 @@ class LandingPageView(TemplateView):
         return render(request, self.template_name)
 
 
-class HomeView(View):
-    def get(self, request):
-        pass
+class HomeView(LoginRequiredMixin, View):
+
+    def __init__(self):
+        self.exercise_random = Exercise.objects.order_by('?').first()
+        self.exercises = Exercise.objects.all()
+        self.tags = Tag.objects.all()
+        self.levels = Exercise.LEVELS
+
+    def get(self, request, *args, **kwargs):
+        level_before = request.user.level
+
+        # handle additional arguments
+        if request.GET.get("tag"):
+            self.exercises = self.exercises.filter(
+                tags__slug=request.GET["tag"])
+            kwargs['filter_type'] = request.GET['tag']
+
+        # handle achievements
+        achievements = achievement_check(request)
+        if achievements:
+            kwargs["achievements"] = []
+            for achievement in achievements:
+                exp_points = achievement.reward_points
+
+                request.user.experience_pts += exp_points
+                request.user.badges.add(achievement)
+
+                kwargs["achievements"].append(achievement)
+            request.user.save()
+
+        # handle new level
+        level_after = request.user.level
+
+        if level_before != level_after:
+            kwargs["new_level"] = True
+
+        return render(request, "exercise/home.html", {
+            'exercises': self.exercises,
+            'exercise_random': self.exercise_random,
+            'tags': self.tags,
+            'levels': self.levels,
+            **kwargs
+        })
+
+    def post(self, request, *args, **kwargs):
+
+        if 'filter' in request.POST:
+            tag = request.POST.get("tag")
+            level = request.POST.get("level")
+
+            if tag == 'all' and level == 'all':
+                return self.get(request)
+            elif tag == 'all':
+                self.exercises = self.exercises.filter(level=level)
+                message = self.levels[int(level) - 1][1]
+            elif level == 'all':
+                self.exercises = self.exercises.filter(tags__id=tag)
+                message = self.tags.get(pk=tag).name
+            else:
+                self.exercises = self.exercises.filter(
+                    tags__id=tag, level=int(level))
+                message = "{} and {}".format(
+                    self.levels[int(level) - 1][1],
+                    self.tags.get(pk=tag).name
+                )
+            kwargs['filter_type'] = message
+
+        if 'search' in request.POST:
+            search_phrase = request.POST.get("phrase")
+
+            self.exercises = self.exercises.filter(
+                Q(title__icontains=search_phrase) |
+                Q(short_description__icontains=search_phrase) |
+                Q(description__icontains=search_phrase)
+            )
+            kwargs['filter_type'] = search_phrase
+
+        return render(request, "exercise/home.html", {
+            'exercises': self.exercises,
+            'exercise_random': self.exercise_random,
+            'tags': self.tags,
+            'levels': self.levels,
+            **kwargs
+        })
+
+    def handle_no_permission(self):
+        return redirect(reverse("index"))
