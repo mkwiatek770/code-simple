6aa100d4dcbc9801c74c5c79a68b94a513423843
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-28 11:35:09 +0100

    Added urls: /users/login/   /users/logout/  /users/signup/

--- codesimple/users/urls.py
+++ codesimple/users/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+from users import views
+
 
 urlpatterns = [
-
+    path("login/", views.signin, name="login"),
+    path("logout/", views.logoutuser, name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
