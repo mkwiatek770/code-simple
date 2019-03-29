d1dd71d42be499bf14ef28a0d9bc499a26531f71
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-29 09:46:48 +0100

    Added serializers CodeSerializer, ExerciseUserSerializer, ExerciseUserSubmitSerializer

---
+++ codesimple/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from exercise.models import ExerciseUser
+
+
+class CodeSerializer(serializers.Serializer):
+    """Serializer for code written by user"""
+
+    code = serializers.CharField()
+    language = serializers.CharField()
+    container_name = serializers.CharField()
+
+
+class ExerciseUserSerializer(serializers.Serializer):
+    """Serializer for receiving informations after running tests"""
+    solved = serializers.BooleanField()
+    tests = serializers.ListField(
+        child=serializers.ListField(
+            child=serializers.CharField(),
+        )
+    )
+    traceback = serializers.CharField(allow_blank=True, allow_null=True)
+
+
+class ExerciseUserSubmitSerializer(serializers.Serializer):
+    message = serializers.CharField(allow_blank=True, allow_null=True)
