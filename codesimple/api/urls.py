1c329bc539a5e2d88a8761e5c70887a721ca8fb3
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-29 10:11:15 +0100

    Modified exercise.html to display Traceback in a better way. Added endpoint for exercise to communicate with frontend application

--- codesimple/api/urls.py
+++ codesimple/api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
          views.add_comment, name="add_comment"),
     path("exercises/<int:exercise_id>/<rate_type>/",
          views.rate_exercise, name="rate_exercise"),
+    path("exercises/<int:exercise_id>/<int:user_id>",
+         views.CodeAPIView.as_view(), name="exercises_user"),
 ]
