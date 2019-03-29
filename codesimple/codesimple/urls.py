bb670abc2528ea25dcdee5bd3ceff8bf0cf26f75
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-03-29 09:09:43 +0100

    Added url for api application in main urls.py file. Added two urls for adding comment and like/dislike exercise

--- codesimple/codesimple/urls.py
+++ codesimple/codesimple/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r"^", include("exercise.urls")),
+    re_path("api/", include("api.urls")),
     re_path("users/", include("users.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
